c370428ee8c4acdf7bdc8e9bcc6b12532f1dd036
Ibuki Yamaguchi <ibukiyamaguchi@gmail.com>
2024-12-29 22:54:21 +0900

    フォルダ構成の変更

--- app/main.py
+++ app/main.py
@@ -3,32 +3,36 @@ from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import uvicorn
+import os
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory=os.path.join(os.getcwd(), "app/static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(os.getcwd(), "app/templates"))
 
-@app.get("/", response_class=HTMLResponse)
+
+@ app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     print('Request for index page received')
     return templates.TemplateResponse('index.html', {"request": request})
 
-@app.get('/favicon.ico')
+
+@ app.get('/favicon.ico')
 async def favicon():
     file_name = 'favicon.ico'
-    file_path = './static/' + file_name
+    file_path = './app/static/' + file_name
     return FileResponse(path=file_path, headers={'mimetype': 'image/vnd.microsoft.icon'})
 
-@app.post('/hello', response_class=HTMLResponse)
+
+@ app.post('/hello', response_class=HTMLResponse)
 async def hello(request: Request, name: str = Form(...)):
     if name:
         print('Request for hello page received with name=%s' % name)
-        return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
+        return templates.TemplateResponse('hello.html', {"request": request, 'name': name})
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
         return RedirectResponse(request.url_for("index"), status_code=status.HTTP_302_FOUND)
 
+
 if __name__ == '__main__':
     uvicorn.run('main:app', host='0.0.0.0', port=8000)
-
